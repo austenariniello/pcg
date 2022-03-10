2454e6a36b0e66708aa238e9faba02c3c56db662
Austen Ariniello <ama1105@nyu.edu>
2022-03-09 22:59:20 -0500

    player movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+
+    private float horizontal;
+    private float vertical;
+    private float speed = 10.0f;
+    Rigidbody2D rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        GetPlayerInput();
+        MovePlayer();
+
+    }
+
+    private void GetPlayerInput() {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer() {
+        Vector3 directionVector = new Vector3(horizontal, vertical, 0);
+        rb.velocity = directionVector.normalized * speed;
     }
 }
