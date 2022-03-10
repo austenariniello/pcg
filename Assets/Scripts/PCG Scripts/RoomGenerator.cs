22bd06f02e9912ea9522f1485ac5ec105d6868cd
Elvis Cabreja <ec3539@nyu.edu>
2022-03-09 19:13:07 -0500

    Allowed for rooms in binary partition to be random walk generated

--- Assets/Scripts/PCG Scripts/RoomGenerator.cs
+++ Assets/Scripts/PCG Scripts/RoomGenerator.cs
@@ -28,7 +28,15 @@ public class RoomGenerator : RandomWalkGenerator
         var roomList = ProceduralGenerationAlgorithms.BinarySpacePartitioning(new BoundsInt((Vector3Int)startPosition, new Vector3Int(dungeonWidth, dungeonHeight, 0)), minRoomWidth, minRoomHeight);
 
         HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
-        floor = CreateSimpleRooms(roomList);
+
+        if (randomWalkRooms)
+        {
+            floor = CreateRoomsRandomly(roomList);
+        }
+        else
+        {
+            floor = CreateSimpleRooms(roomList);
+        }
 
         List<Vector2Int> roomCenters = new List<Vector2Int>();
         foreach (var room in roomList)
@@ -41,6 +49,28 @@ public class RoomGenerator : RandomWalkGenerator
 
         tilemapVisualizer.PaintFloorTiles(floor);
         WallGenerator.CreateWalls(floor, tilemapVisualizer);
+    }
+
+    private HashSet<Vector2Int> CreateRoomsRandomly(List<BoundsInt> roomList)
+    {
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            var roomBounds = roomList[i];
+
+            var roomCenter = new Vector2Int(Mathf.RoundToInt(roomBounds.center.x), Mathf.RoundToInt(roomBounds.center.y));
+
+            var roomFloor = RunRandomWalk(randomWalkParameters, roomCenter);
+
+            foreach (var position in roomFloor)
+            {
+                if (position.x >= (roomBounds.xMin + offset) && position.x <= (roomBounds.xMax - offset) && position.y >= (roomBounds.yMin - offset) && position.y <= (roomBounds.yMax - offset))
+                {   // checks if it satisfies the offset
+                    floor.Add(position);
+                }
+            }
+        }
+        return floor;
     }
 
     private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)
